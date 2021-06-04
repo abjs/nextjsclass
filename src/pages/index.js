d84b829abf6edb4d2a20b8349bbacb088e5ecbc9
Abin Joseph <itsmeabjs@gmail.com>
2021-06-04 18:18:01 +0530

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
 import styles from "@/styles/Home.module.css";
 export default function Home() {
+  function Submit(event) {
+    event.preventDefault();
+    const data = event.target.todo.value;
+    event.target.todo.value="";
+    console.log(data);
+  }
   return (
     <div className={styles.main}>
-      <h1>Name</h1>
+      <form onSubmit={Submit}>
+        <input
+          type="text"
+          name="todo"
+          className={styles.input}
+          placeholder="Enter Todo"
+        />
+        <button type="submit" className={styles.button}>
+          Add Todo
+        </button>
+      </form>
     </div>
   );
 }
