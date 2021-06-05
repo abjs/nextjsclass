3e99f21daa03788a799f5094a00c13d69a2e62d8
Abin Joseph <itsmeabjs@gmail.com>
2021-06-05 18:30:04 +0530

    Todo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+
 export default function Home() {
-  function Submit(event) {
+  const Submit = (event) => {
     event.preventDefault();
     const data = event.target.todo.value;
-    event.target.todo.value="";
-    console.log(data);
-  }
+    event.target.todo.value = "";
+    settodo([...todo,data]);
+  };
+  const [todo, settodo] = useState([]);
   return (
     <div className={styles.main}>
       <form onSubmit={Submit}>
@@ -19,6 +22,23 @@ export default function Home() {
           Add Todo
         </button>
       </form>
+
+
+      {/* {
+        todo.map(function(data,index) {
+          return(
+            <h1 key={index} >{data}</h1>
+          )
+        })
+      } */}
+
+      {
+        todo.map((data,index)=><h1 key={index}>{data}</h1>)
+      }
+
+
+
+
     </div>
   );
 }
